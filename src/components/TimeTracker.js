b9495d78ab7c90f13889572b64d96ac7733afd8c
Diego Muracciole <diegomuracciole@gmail.com>
2016-11-20 19:31:13 -0300

    Add timer

--- src/components/TimeTracker.js
+++ src/components/TimeTracker.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
+import secToMin from 'sec-to-min';
 import './TimeTracker.scss';
 
 class TimeTracker extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = { running: false, timer: 0 };
+
+    this.toggleTimer = ::this.toggleTimer;
+  }
+
+  toggleTimer () {
+    if (this.state.running) {
+      this.stopTimer();
+    } else {
+      this.startTimer();
+    }
+  }
+
+  startTimer () {
+    this.setState({ running: true });
+
+    this.timerInterval = setInterval(() => {
+      this.setState({ timer: this.state.timer + 1 });
+    }, 1000);
+  }
+
+  stopTimer () {
+    this.setState({ running: false, timer: 0 });
+
+    clearInterval(this.timerInterval);
+  }
+
   render () {
+    const { running, timer } = this.state;
+    const buttonText = running ? 'Stop' : 'Start';
+
     return (
       <div className='time-tracker'>
         <input type='text' />
-        <div>00:01</div>
-        <button>Start</button>
+        <div>{secToMin(timer)}</div>
+        <button onClick={this.toggleTimer}>{buttonText}</button>
       </div>
     );
   }
