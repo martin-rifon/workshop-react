c1203608f9d301b45cca3933616bbb9fb6787b11
Diego Muracciole <diegomuracciole@gmail.com>
2016-11-23 09:57:09 -0300

    Persist user session

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,19 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
-import {initialState, reducer} from './reducer';
+import { initialState, reducer } from './reducer';
+import { loadState, saveState } from './stateStorage';
 
+const persistedState = loadState();
 const middleware = [thunk];
 
 const store = createStore(
   reducer,
-  initialState,
+  Object.assign({}, initialState, persistedState),
   applyMiddleware(...middleware)
 );
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
 
 export default store;
